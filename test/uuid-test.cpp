7a6e078896a316bd1263d6db8120e371da531c57
John Carr <john.carr@ufl.edu>
2023-02-05 12:17:49 -0600

    Add bug report

--- test/uuid-test.cpp
+++ test/uuid-test.cpp
@@ -9,8 +9,10 @@
 TEST(UUIDTests, SunnyCase)
 {
   static const UUID test = "89ede841-12aa-49e0-8727-79f2ce8552df"_uuid;
-  static const std::array<uint8_t, 16> expect = {0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
-                                    0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF};
+  static const std::array<uint8_t, 16> expect = {
+                                0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
+                                0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF
+                                };
   ASSERT_EQ(test, expect);
 }
 
@@ -90,7 +92,8 @@ TEST(UUIDTests, EqOpGood)
   static const UUID test2 = "89ede841-12aa-49e0-8727-79f2ce8552df"_uuid;
   static const std::array<uint8_t, 16> expect = {
                                 0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
-                                0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF};
+                                0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF
+                                };
   bool test_result_arr = (test == expect);
   bool test_result_uuid = (test == test2);
   ASSERT_TRUE(test_result_arr);
@@ -103,7 +106,8 @@ TEST(UUIDTests, EqOpBad)
   static const UUID test2 = "89ede841-12aa-49e0-8727-79f2ce8552BB"_uuid;
   static const std::array<uint8_t, 16> expect = {
                                 0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
-                                0x87, 0x27, 0x79, 0xF2, 0xBB, 0x85, 0x52, 0xDF};
+                                0x87, 0x27, 0x79, 0xF2, 0xBB, 0x85, 0x52, 0xDF
+                                };
   bool test_result_arr = (test == expect);
   bool test_result_uuid = (test == test2);
   ASSERT_FALSE(test_result_arr);
@@ -116,7 +120,8 @@ TEST(UUIDTests, InEqOpGood)
   static const UUID test2 = "89ede841-12aa-49e0-8727-79f2ce8552AA"_uuid;
   static const std::array<uint8_t, 16> expect = {
                                 0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
-                                0x87, 0x27, 0x79, 0xF2, 0xBB, 0x85, 0x52, 0xDF};
+                                0x87, 0x27, 0x79, 0xF2, 0xBB, 0x85, 0x52, 0xDF
+                                };
   bool test_result_arr = (test != expect);
   bool test_result_uuid = (test != test2);
   ASSERT_TRUE(test_result_uuid);
@@ -128,6 +133,7 @@ TEST(UUIDTests, AccessOp)
   static const UUID test = "89ede841-12aa-49e0-8727-79f2ce8552df"_uuid;
   static const std::array<uint8_t, 16> expect = {
                                 0x89, 0xED, 0xE8, 0x41, 0x12, 0xAA, 0x49, 0xE0,
-                                0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF};
+                                0x87, 0x27, 0x79, 0xF2, 0xCE, 0x85, 0x52, 0xDF
+                                };
   ASSERT_EQ(expect[0], test[0]);
 }
