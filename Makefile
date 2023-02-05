cf573fc7a045d3f58f3ce4f8ba7b12cd38129127
John Carr <john.carr@ufl.edu>
2023-02-04 19:30:57 -0600

    Add more operators to uuid

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ all:
 run-test:
 	cd build && ctest && cd ..
 
+compile-cov:
+	cd build && lcov -c --directory . --output-file main_coverage.info && genhtml main_coverage.info --output-directory out && cd ..
+
 clean:
 	rm -rf build
--- inc/uuid.hpp
+++ inc/uuid.hpp
@@ -7,14 +7,40 @@
 
 class UUID
 {
-
-public:
+private:
     std::array<uint8_t, 16> mData = {0};
 
+public:
+    // Constructor
     UUID() = default;
+    [[nodiscard]] uint8_t& operator[](size_t);
+    // Friends
+    [[nodiscard]] friend bool operator==(const UUID& L, const std::array<uint8_t, 16>& R) noexcept;
+    [[nodiscard]] friend bool operator==(const UUID& L, const UUID& R) noexcept;
+    [[nodiscard]] friend bool operator!=(const UUID& L, const UUID& R) noexcept;
     friend constexpr UUID operator""_uuid(const char* text, size_t len);
 
 };
+
+uint8_t& UUID::operator[](size_t index)
+{
+    return mData[index];
+}
+
+bool operator==(const UUID& L, const std::array<uint8_t, 16>& R) noexcept
+{
+    return L.mData == R;
+}
+
+bool operator==(const UUID& L, const UUID& R) noexcept
+{
+    return L.mData == R.mData;
+}
+
+bool operator!=(const UUID& L, const UUID& R) noexcept
+{
+    return L.mData != R.mData;
+}
 
 constexpr UUID operator""_uuid(const char* text, size_t len)
 {
@@ -34,9 +60,7 @@ constexpr UUID operator""_uuid(const char* text, size_t len)
             continue;
         }
         int8_t upper = hexToByte(text[index++]);
-        // index++;
         int8_t lower = hexToByte(text[index++]);
-        // index++;
         // Check if it was an invalid character
         if( upper < 0 || lower < 0)
         {
